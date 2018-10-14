27923db1d26cb48d0bc0c062ecb6c3addaf8b04d
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-10-14 06:53:41 +0530

    add custom message for validation

--- app/validators/HelloValidator.py
+++ app/validators/HelloValidator.py
@@ -5,7 +5,10 @@ from validator import Length, Required
 class HelloValidator(Validator):
 
     def hello_form(self):
-        self.validate({
+        self.messages({
+            'name': 'Your name is not long enough!'
+        })
+        return self.validate({
             'name': [Required, Length(3)]
         })
 
