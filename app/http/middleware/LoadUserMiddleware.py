bba342c32cfc890df0075b011bf6cff88ca09887
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-12-04 11:35:45 +0530

    resolve dependancy via annotation for middleware

--- app/http/middleware/LoadUserMiddleware.py
+++ app/http/middleware/LoadUserMiddleware.py
@@ -1,12 +1,14 @@
 ''' Load User Middleware'''
+
 from masonite.auth import Auth
+from masonite.request import Request
 
 class LoadUserMiddleware:
     ''' Middleware class which loads the current user into the request '''
 
-    def __init__(self, Request):
+    def __init__(self, request: Request):
         ''' Inject Any Dependencies From The Service Container '''
-        self.request = Request
+        self.request = request
 
     def before(self):
         ''' Run This Middleware Before The Route Executes '''
